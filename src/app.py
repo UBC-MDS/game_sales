88ee9e287bf771dd220a636b06c10f0a87734f8e
khalidcawl <khalidca@student.ubc.ca>
2022-03-03 23:30:36 -0800

    fix Procfile

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ import numpy as np
 import dash_bootstrap_components as dbc
 
 import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../line_charts')))
 
-sys.path.append("/app/")
-print(sys.path)
 from line_charts import *
 from publishers import *
 
