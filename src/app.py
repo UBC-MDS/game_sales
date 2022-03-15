ce9ac472bfa1f9278333b2f2c0037946c95ed086
Zheren Xu <xuzheren1998@gmail.com>
2022-03-14 18:38:57 -0700

    Remove title

--- src/app.py
+++ src/app.py
@@ -49,21 +49,17 @@ def update_output(value):
 def lineGlobalFigure():
     return  html.Iframe(
                     id=f'line_global',
-                    style={'border-width': '0', 'width': '100%', 'height': '400px'},
                     srcDoc=plotLineGlobal(genre='Sports'))
-
 
 def lineGenreFigure():
     return  html.Iframe(
                     id=f'line_genre',
-                    style={'border-width': '0', 'width': '100%', 'height': '400px'},
                     srcDoc=plotLineGenre(genre='Sports', region='North America'))
 
 def publishersFigure():
     return  html.Div([
         html.Iframe(
                     id=f'publishers',
-                    style={'border-width': '0', 'width': '100%', 'height': '400px'},
                     srcDoc=plotPublishers(top_val='10'))
     ])
 
@@ -71,31 +67,13 @@ def platformsFigure():
     return  html.Div([
         html.Iframe(
                     id=f'platforms',
-                    style={'border-width': '0', 'width': '100%', 'height': '400px'},
                     srcDoc=plotPublishers(top_val='15'))
     ])
 
 app.title = "Video Game Sales"
 
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "20rem",
-    "padding": "2rem 1rem",
-}
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
-
 sidebar = html.Div([
-        html.H2('Video Game Sales'),
+        html.H2('Menu'),
         html.Br(),
         html.H5('Genre'),
         dcc.Dropdown(
@@ -119,9 +97,10 @@ sidebar = html.Div([
                value=15,
                id='platforms-slider')
         ],
-    id="sidebar", style=SIDEBAR_STYLE)
+    id="sidebar")
 
 content = html.Div([
+                html.H1("Video Game Sales", id='title'),
                 dbc.Row([
                         dbc.Col([
                             lineGlobalFigure()
@@ -140,7 +119,7 @@ content = html.Div([
                         ]),
                 ]),
             ],
-    id="page-content", style=CONTENT_STYLE)
+    id="page-content")
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
